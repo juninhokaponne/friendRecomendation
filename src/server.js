a603631189e1ede6b9b2bff48f67f27a8a245754
Gilson Oliveira <juninhokaponnemg@hotmail.com>
2023-09-23 17:03:32 -0300

    feat: added .env file

--- src/server.js
+++ src/server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const routes = require('./routes/routes');
 const app = express();
 app.use(express.json());
+require('dotenv').config();
 
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`Server running on port ${process.env.PORT || 4000} `);
 });
