b6ebeb8950a8649f110d2ee1ee7f5c2605179a5c
Gilson Oliveira <juninhokaponnemg@hotmail.com>
2023-09-25 21:50:02 -0300

    refactor: routes refactor issue #1

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,5 @@
 const express = require("express");
-
 const router = express.Router();
-
 const PersonController = require("../controller/PersonController");
 const RelationshipController = require("../controller/RelationshipController");
 
@@ -11,35 +9,14 @@ const relationshipController = new RelationshipController();
 const validateCPF = require("../middleware/validateCPF");
 router.use(express.json());
 
-router.post("/person", validateCPF, (req, res) => {
-  const { cpf, name } = req.body;
-  const result = personController.createPerson(cpf, name);
-  res.status(result.error ? 400 : 200).send(result);
-});
-
-router.get("/person/:cpf", (req, res) => {
-  const { cpf } = req.params;
-  const person = personController.getPerson(cpf);
-  res.status(person.error ? 404 : 200).json(person);
-});
-
-router.delete("/clean", (req, res) => {
-  const result = personController.cleanPeople();
-  res.status(200).send(result);
-});
-
-router.post("/relationship", (req, res) => {
-  const { cpf1, cpf2 } = req.body;
-  relationshipController.setPeople(personController.people);
-  const result = relationshipController.createRelationship(cpf1, cpf2);
-  res.status(result.error ? 400 : 200).json(result);
-});
-
-router.get("/recommendations/:cpf", validateCPF, (req, res) => {
-  const { cpf } = req.params;
-  relationshipController.setPeople(personController.people);
-  const recommendations = relationshipController.getRecommendations(cpf);
-  res.status(recommendations.error ? 400 : 200).json(recommendations);
-});
+router.post("/person", validateCPF, personController.createPersonRoute);
+router.get("/person/:cpf", personController.getPersonRoute);
+router.delete("/clean", personController.cleanPeopleRoute);
+router.post("/relationship", relationshipController.createRelationshipRoute);
+router.get(
+  "/recommendations/:cpf",
+  validateCPF,
+  relationshipController.getRecommendationsRoute,
+);
 
 module.exports = router;
